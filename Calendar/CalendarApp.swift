c0e9e4a2e8b241a60a91e7d678ce624b243e6b7f
Raphael Huang-Dubois <raphael.huangdubois@gmail.com>
2023-09-22 16:59:54 +0200

    Initial Commit

---
+++ Calendar/CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarApp.swift
+//  Calendar
+//
+//  Created by Raphaël Huang-Dubois on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
